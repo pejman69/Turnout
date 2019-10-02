1249e69c1b9bf21dc4517711fafb15384177d212
pejman <pejmanmowloudi@gmail.com>
2019-10-02 10:30:09 +0330

    Add user and create form for email, password

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="">
-  Sign In Component
+  <div class="form-inline">
+<div class="form-group">
+  <h3>Sign In</h3>
+  <input
+  type="text"
+  placeholder="email"
+  class="form-control"
+  v-model="email"
+  />
+  <input
+  type="password"
+  placeholder="password"
+  class="form-control"
+  v-model="password"
+  />&nbsp;
+  <button class="btn btn-primary" @click="signIn">Sign In</button>
+</div>
+<br>
+<p>{{error.message}}</p>
+<br>
   </div>
 </template>
+<script>
+import { firebaseApp } from '../firebaseApp'
+
+export default {
+  name: "",
+  data() {
+return {
+  email:'',
+  password:'',
+  error: {
+    message:''
+  }
+}
+},
+methods:{
+  signIn(){
+    firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+.catch(error => {
+  this.error = error
+})
+  }
+}
+}
+</script>
